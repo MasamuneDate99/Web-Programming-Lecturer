f7c76b68cf4777ee49634e08fd2d17a411067f62
ancip <antoniusciptawijaya@yahoo.com>
2022-01-18 19:57:01 +0700

    paginate sm home

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Image;
 
 class ImageController extends Controller
 {
     //
     public function home(){
+        $images = Image::paginate(6);
         return view('home');
+    }
+
+    public function searchImage(Request $request){
+        $images = Image::where('name','like','%'.$request->search.'%')->paginate(6);
+        return view('search_image',compact('images'));
     }
 
 }
