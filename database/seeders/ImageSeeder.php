d428b2db2262d943e58e9315ed82701dd72d46fb
ancip <antoniusciptawijaya@yahoo.com>
2022-01-18 20:51:19 +0700

    imeges

--- database/seeders/ImageSeeder.php
+++ database/seeders/ImageSeeder.php
@@ -17,47 +17,47 @@ class ImageSeeder extends Seeder
         DB::table('images')->insert([
             'name' => 'Foto Keluarga 1',
             'description' => 'Foto Keluarga 1',
-            'image' => 'storage/image_1.jpg',
+            'image' => 'display_images/image_1.jpg',
         ]);
         DB::table('images')->insert([
             'name' => 'Foto Keluarga 2',
             'description' => 'Foto Keluarga 2',
-            'image' => 'storage/image_2.png',
+            'image' => 'display_images/image_2.png',
         ]);
         DB::table('images')->insert([
             'name' => 'Foto Keluarga 3',
             'description' => 'Foto Keluarga 3',
-            'image' => 'storage/image_3.jpg',
+            'image' => 'display_images/image_3.jpg',
         ]);
         DB::table('images')->insert([
             'name' => 'Foto Keluarga 4',
             'description' => 'Foto Keluarga 4',
-            'image' => 'storage/image_4.jpg',
+            'image' => 'display_images/image_4.jpg',
         ]);
         DB::table('images')->insert([
             'name' => 'Foto Keluarga 5',
             'description' => 'Foto Keluarga 5',
-            'image' => 'storage/image_5.jpg',
+            'image' => 'display_images/image_5.jpg',
         ]);
         DB::table('images')->insert([
             'name' => 'Foto Keluarga 6',
             'description' => 'Foto Keluarga 6',
-            'image' => 'storage/image_6.jpg',
+            'image' => 'display_images/image_6.jpg',
         ]);
         DB::table('images')->insert([
             'name' => 'Foto Keluarga 7',
             'description' => 'Foto Keluarga 7',
-            'image' => 'storage/image_7.jpg',
+            'image' => 'display_images/image_7.jpg',
         ]);
         DB::table('images')->insert([
             'name' => 'Foto Keluarga 8',
             'description' => 'Foto Keluarga 8',
-            'image' => 'storage/image_8.jpg',
+            'image' => 'display_images/image_8.jpg',
         ]);
         DB::table('images')->insert([
             'name' => 'Foto Keluarga 9',
             'description' => 'Foto Keluarga 9',
-            'image' => 'storage/image_9.jpg',
+            'image' => 'display_images/image_9.jpg',
         ]);
     }
 }
